e00ea8688ab624744ddc36ab657c475696ebc9b4
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-02-22 13:05:34 +0530

    mongodb conntected

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log("Server Started")
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./db/config.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.listen(port,()=>{
+    console.log(`Server is Running on port: ${port} `)
+    connectDb()
+})
