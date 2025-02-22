eb056596a5d11692f3090b259713e3e1c538da88
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-02-22 14:27:27 +0530

    resignation schema completed

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./db/config.js";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes.js"
+import adminRoutes from "./routes/admin.routes.js"
 import userRoutes from "./routes/user.routes.js"
 
 dotenv.config();
@@ -11,7 +13,9 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth",userRoutes);
+app.use("/api/auth",authRoutes);
+app.use("/api/admin",adminRoutes);
+app.use("/api/user",userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is Running on port: ${port} `);
